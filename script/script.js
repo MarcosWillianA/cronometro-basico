f1b337c22e809e0e34e42cde944b1f99c531f744
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-07 15:44:54 -0300

    Javascript
    Javascript concluído

---
+++ script/script.js
@@ -0,0 +1,47 @@
+const display = document.querySelector('#display');
+const start = document.querySelector('#start');
+const pause = document.querySelector('#pause');
+const reset = document.querySelector('#reset');
+
+let [milissegundos, segundos, minutos, horas] = [0, 0, 0, 0];
+let int;
+
+start.addEventListener('click', () => {
+    int = setInterval(mostrarCronometro, 10);
+})
+
+pause.addEventListener('click', () => {
+    clearInterval(int);
+})
+
+reset.addEventListener('click', () => {
+    clearInterval(int);
+    [milissegundos, segundos, minutos, horas] = [0, 0, 0, 0];
+    display.innerHTML = '00 : 00 : 00 : 000';
+})
+
+function mostrarCronometro() {
+    milissegundos+=10;
+    if (milissegundos === 1000) {
+        milissegundos = 0;
+        segundos++;
+        if (segundos === 60) {
+            segundos = 0;
+            minutos++;
+            if (minutos === 60) {
+                minutos = 0;
+                horas++;
+            }
+        }
+    }
+    let h = horas < 10 ? '0' + horas : horas;
+    let m = minutos < 10 ? '0' + minutos : minutos;
+    let s = segundos < 10 ? '0' + segundos : segundos;
+    let ms = milissegundos < 10 ? '00' + milissegundos : milissegundos;
+    milissegundos < 100 ? '0' + milissegundos : milissegundos;
+
+    display.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
+
+
+
